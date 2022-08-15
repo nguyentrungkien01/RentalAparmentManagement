5cccd6bf25cf820fb33d8ee63528850ced729421
namnguyenthanhwork <namnguyenthanh.work@gmail.com>
2022-08-15 19:37:08 +0700

    do: remove submodule

--- rental-aparment-ui/src/components/Layout.jsx
+++ rental-aparment-ui/src/components/Layout.jsx
@@ -1,7 +1,29 @@
 import React from 'react';
 
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+import Header from './Header';
+import Footer from './Footer';
+import { publicRoutes } from '../routes/routes';
 const Layout = () => {
-    return <div>Layout</div>;
+    return (
+        <>
+            <div className="container">
+                <Header />
+                <div className="main">
+                    <BrowserRouter>
+                        <Routes>
+                            {publicRoutes.map((route, index) => {
+                                const Page = route.component;
+                                return <Route key={index} path={route.path} element={<Page />} />;
+                            })}
+                        </Routes>
+                    </BrowserRouter>
+                </div>
+            </div>
+            <Footer />
+        </>
+    );
 };
 
 export default Layout;
