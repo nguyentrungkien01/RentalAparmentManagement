4bea3f2aff0f1a6712cb4bb25b9d9168ead97998
nguyenvantruong-ou <1951052220truong@gmail.com>
2022-08-20 10:34:40 +0700

    rating

---
+++ RentalApartmentManagement/DTO/Request/RatingRequest.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DTO.Request
+{
+    public class RatingRequest : IBaseRequest
+    {
+        public int PostId { get; set; }
+        public int RateAmount { get; set; }
+        public string PhoneNumber { get; set; }
+    }
+}
