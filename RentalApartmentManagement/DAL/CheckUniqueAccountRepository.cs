9285a045cec25b2a0fa0fc2d4ffbcbb6273deab1
nguyenvantruong-ou <1951052220truong@gmail.com>
2022-08-21 19:52:19 +0700

    register

---
+++ RentalApartmentManagement/DAL/CheckUniqueAccountRepository.cs
@@ -0,0 +1,42 @@
+﻿using DAL.Entity;
+using DTO.Request;
+using DTO.Respone;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DAL
+{
+    public class CheckUniqueAccountRepository 
+    {
+        RentalApartmentManagementContext _dtContext;
+
+        public CheckUniqueAccountRepository()
+        {
+            _dtContext = new RentalApartmentManagementContext();
+        }
+
+        public Boolean CheckPhoneNumber(string phoneNumber)
+        {
+            if (_dtContext.Account.FirstOrDefault(s => s.PhoneNumber == phoneNumber) is null)
+                return false;
+            return true;
+        }
+
+        public Boolean CheckIdCard(string idCard)
+        {
+            if (_dtContext.Account.FirstOrDefault(s => s.IdCard == idCard) is null)
+                return false;
+            return true;
+        }
+
+        public Boolean CheckEmail(string email)
+        {
+            if (_dtContext.Account.FirstOrDefault(s => s.Email == email) is null)
+                return false;
+            return true;
+        }
+
+    }
+}
---
+++ RentalApartmentManagement/DAL/RegisterRepository.cs
@@ -0,0 +1,59 @@
+﻿using DTO.Request;
+using DTO.Respone;
+using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Text;
+using Util.Constant;
+using DAL.Entity;
+
+namespace DAL
+{
+    public class RegisterRepository : BaseRepository<IBaseRequest, IBaseResponse>
+    {
+        protected override IBaseResponse DoExcute(IBaseRequest input)
+        {
+            var req = (RegisterRequest)input;
+            try
+            {
+                Account acc = new Account();
+                acc.FirstName = req.FirstName;
+                acc.LastName = req.LastName;
+                acc.Gender = req.Gender;
+                acc.Address = req.Address;
+                acc.PhoneNumber = req.PhoneNumber;
+                acc.IdCard = req.IdCard;
+                acc.Email = req.Email;
+                acc.Password = req.Password;
+                acc.RoleId = 1;
+                _dtContext.Account.Add(acc);
+                _dtContext.SaveChanges();
+                return new CommonResponse
+                {
+                    Message = Message.OK,
+                    Code = Code.OK
+                };
+            }
+            catch
+            {
+                return new CommonResponse
+                {
+                    Data = "Save failed",
+                    Message = Message.SQL_EXCEPTION,
+                    Code = Code.NOT_FOUND
+                };
+            }
+        }
+
+       
+        protected override void PostExcute(IBaseRequest input)
+        {
+            //
+        }
+
+        protected override void PreExcute(IBaseRequest input)
+        {
+            //
+        }
+    }
+}
